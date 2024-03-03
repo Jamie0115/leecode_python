a0552a35b09a14ba988f3f7e0de379b57a55fe6a
Jamie0115 <gao880115@gmail.com>
2024-03-03 23:46:59 +0800

    lesson5

---
+++ jamie/lesson/lesson_檔案處理.py
@@ -0,0 +1,17 @@
+#  \n:執行  \t: TAB
+"""
+path = "C:/Users/funny/Documents/leecode_python/files/"
+f = open(path + "demo.txt", "w", encoding="utf-8")  # 開檔, "w"是寫入模式
+content = "Hello" + "\n" + "World"
+f.write(content)
+f.close()  #關檔
+"""
+
+path = "C:/Users/funny/Documents/leecode_python/files/"
+try:
+    f = open(path + "demo.csv", "r", encoding="utf-8")  # 開檔, "r"是讀取模式
+    content = f.read()
+    f.close()  #關檔
+    print(content)
+except FileNotFoundError as e:
+    print("File path is not found !!")
