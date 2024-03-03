1c7c7acc0c75cb996a44b6a2c6d3ac1c6423fcee
EricRMing <ming830330@gmail.com>
2024-03-03 23:50:51 +0800

    lesson4,5

---
+++ eren/lesson/lesson4.py
@@ -0,0 +1,14 @@
+person = {
+    "name": "Eric",
+    "age": 18
+}
+print(person["name"])
+
+person["name"] = "Eren"
+print(person)
+
+person["country"] = "TW"
+print(person)
+
+del person["age"]
+print(person)
---
+++ eren/lesson/lesson5.py
@@ -0,0 +1,19 @@
+# \n: 換行 \t: TAB
+"""
+path = "/Users/ericwu/Documents/workspace/demo/file/"
+f = open(path + "demo.txt", "w", encoding="utf-8")  # 開檔, "w"是寫入模式
+content = "Hello" + "\n" + "World"
+f.write(content)
+f.close()  # 關檔
+"""
+path = "/Users/ericwu/Documents/workspace/demo/file/"
+try:
+    f = open(path + "demo.csv", "r", encoding="utf-8")  # 開檔, "r"是讀取模式
+    content = f.read()
+    f.close()
+    print(content)
+except FileNotFoundError as e:
+    print("File path is not found !!")
+
+
+
