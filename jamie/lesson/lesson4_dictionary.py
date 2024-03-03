10f39a2d5e2634a0706001420a9fa3a3a2563337
Jamie0115 <gao880115@gmail.com>
2024-03-03 23:46:43 +0800

    lesson4

---
+++ jamie/lesson/lesson4_dictionary.py
@@ -0,0 +1,12 @@
+#  dictionary = {key, value}
+person = {"name": "Jamie", "age": 25}
+print(person["name"])
+
+person["name"] = "Eren"
+print(person)
+
+person["country"] = "TW"
+print(person)
+
+del person["age"]
+print(person)
