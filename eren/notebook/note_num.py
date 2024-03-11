ba1c775e0f3e7f6861273b5bb66c4a6f49dcab43
Eren <43052203+gmwu0330@users.noreply.github.com>
2024-03-12 00:22:41 +0800

    practice prime num

---
+++ eren/notebook/note_num.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------
+sample_list = [3, 2, 1, 5, 4]
+
+# abs(n) : 回傳n的絕對值
+print("abs(-10):", abs(-10))
+
+# len(o) : 回傳o的長度(元素個數)，
+# o可以是序列（如 string、bytes、tuple、list、range)或集合（如 dictionary、set、frozen set)
+print("len([3, 2, 1, 5, 4]):", len([3, 2, 1, 5, 4]))
+
+# sum(list) : 對iterable的每項求和並傳回總計值
+print("sum([3, 2, 1, 5, 4]):", sum([3, 2, 1, 5, 4]))
+
---
+++ eren/practice/practice8.py
@@ -0,0 +1,23 @@
+import math
+
+
+# 1. primeNumList [] => 放質數的list
+# 2. range 2~n，每個數字除以primeNumList的每個質數，如果都不能整除，這個數字就是質數
+
+def all_pass(current_list, current_num):
+    for index in current_list:
+        if current_num % index == 0:
+            return False
+        else:
+            pass
+    return True
+
+
+num = int(input("Input number : "))
+
+primeNumList = list()
+for i in range(2, num + 1):
+    if all_pass(primeNumList, i):
+        primeNumList.append(i)
+
+print(primeNumList)
