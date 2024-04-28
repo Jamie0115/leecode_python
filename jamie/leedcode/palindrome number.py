69a71eb58c22593c249b0c5aad85157c529bdff7
Jamie0115 <gao880115@gmail.com>
2024-04-28 14:43:19 +0800

    回文

--- jamie/leedcode/palindrome number.py
+++ jamie/leedcode/palindrome number.py
@@ -1,17 +1,40 @@
 #  1. 判斷是否為回文數字
+#  利用Python的內建 反轉陣列[::-1]
+"""
 def isPalindrome(x):
-    x = int()
-    boo = False
-    result = x
-    temp = 0
-    if x < 0:  # 先判斷是否為正數
-        return boo
-    while result != 0:  # 確認是正數後代入演算法
-        temp = temp * 10 + result % 10
-        result = result // 10
-    if temp == x:
-        boo = True
-    return boo
+    result = str(x)[::-1]
+    if str(x) != result:
+        return False
+    else:
+        return True
 
+x = int(input("Number: "))
+print(isPalindrome(x))
 
-print(isPalindrome(12545456))
+"""
+"""
+numbers = int(input("Number: "))
+newList = str(numbers)[::-1]  # [::-1]是要list的形式
+print(newList)
+"""
+#  回文數簡單就是左到右，右到左皆相同就是回文數了
+x = int(input("Number: "))
+if x < 0:
+    print(False)
+else:
+    array = list()
+    while x > 0:
+        array.append(x % 10)
+        x = x // 10
+        print(array)
+    leftIndex = 0
+    rightIndex = len(array) - 1
+    print(array[leftIndex], array[rightIndex])
+    print(leftIndex, rightIndex)
+    while leftIndex < rightIndex:
+        if array[leftIndex] != array[rightIndex]:
+            print(False)
+        else:
+            leftIndex = leftIndex + 1
+            rightIndex = rightIndex - 1
+    print(True)
