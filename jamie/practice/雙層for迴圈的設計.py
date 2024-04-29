431fdeb8fbc1f6ce284714a49b938524e7867240
Jamie0115 <gao880115@gmail.com>
2024-04-30 00:28:38 +0800

    練習1

--- jamie/practice/雙層for迴圈的設計.py
+++ jamie/practice/雙層for迴圈的設計.py
@@ -27,7 +27,30 @@ def starTree(n):
 n = int(input("Number: "))
 starTree(n)
 """
+"""
 sum = 0
 for I in range(-5, 101, 7):
-    print(I)
     sum = sum + 1
+    print(sum)
+"""
+"""
+value = 100
+sum = 0
+while sum < 300:
+    value -= 20
+    if value < 0:
+        break
+    sum += value
+print(sum)
+"""
+"""
+def F(x, y):
+    k = 0
+    for i in range(x, 0, -1):
+        print(i)
+        for j in range(y, 0, -1):
+            print(j)
+            k = k + 1
+    return k
+print(F(3, 1))
+"""
