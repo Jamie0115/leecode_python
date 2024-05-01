3727c5da39948362ae757d915920e462f3fe5085
Eren <43052203+gmwu0330@users.noreply.github.com>
2024-05-01 12:23:33 +0800

    prefix common

--- eren/leetcode/14.LongestCommonPrefix.py
+++ eren/leetcode/14.LongestCommonPrefix.py
@@ -3,10 +3,23 @@ def longestCommonPrefix(strs):
     :type strs: List[str]
     :rtype: str
     """
-    for s in strs[1:]:
-        print(s)
-
-    return strs
+    if len(strs) == 0:
+        return ""
+    elif len(strs) == 1:
+        return strs[0]
+    else:
+        tempString = strs[0]
+        for i in range(1, len(strs)):
+            currentString = strs[i]
+            tempString = strs[i] if len(currentString) < len(tempString) else tempString
+            tempSize = 0
+            for word in range(len(tempString)):
+                if tempString[word] == currentString[word]:
+                    tempSize +=1
+                else:
+                    break
+            tempString = tempString[:tempSize]
+        return tempString
 
 
 print(longestCommonPrefix(["flower", "flow", "flight"]))
