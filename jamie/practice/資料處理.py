a6f239e98bc77d4e134978530e3853e19266b1d4
Jamie0115 <gao880115@gmail.com>
2024-06-08 22:07:52 +0800

    資料處理

--- jamie/practice/資料處理.py
+++ jamie/practice/資料處理.py
@@ -1,10 +1,32 @@
 import csv
-test1_path = "C:/Users/funny/Desktop/test1.csv"
-test2_path = "C:/Users/funny/Desktop/test2.csv"
-result_path = '/path/to/directory3/result.csv'
-# 讀取 file1.csv
+test1_path = "C:/Users/funny/Desktop/count/test1.csv"
+test2_path = "C:/Users/funny/Desktop/count/test2.csv"
+count_path = "C:/Users/funny/Desktop/count"
+# 讀取 test1.csv
+resultMap1 = dict()
 with open(test1_path, mode='r', newline='') as test1:
-    reader1 = csv.reader(test1_path)
-    next(reader1)  # 跳過標題行
-    row1 = next(reader1)  # 第一筆資料
-    value1 = int(row1[1])  # 取得第一筆資料的 Value 欄位值
+    # reader1 = csv.reader(test1)
+    reader1 = csv.DictReader(test1)
+    for row in reader1:
+        pixel1 = row.get('pixel') # 第一筆資料
+        value1 = row.get('intensity')  # 取得第一筆資料的 Value 欄位值
+        resultMap1[int(pixel1)] = int(value1)
+
+# 讀取 test2.csv
+with open(test2_path, mode='r', newline='') as test2:
+    reader2 = csv.reader(test2)
+    next(reader2)  # 跳過標題行
+    next(reader2)  # 跳過第一筆資料
+    row2 = next(reader2)  # 第二筆資料
+    value2 = int(row2[1])  # 取得第二筆資料的 Value 欄位值
+    print(value2)
+# 計算差值
+value_diff = value1 - value2
+# 寫入結果到新的 CSV 檔案
+with open(count_path, mode='w', newline='') as result_file:
+    writer = csv.writer(result_file)
+    # 寫入標題行
+    writer.writerow(['ID', 'Value_diff'])
+    # 寫入計算結果
+    writer.writerow([row1[0], value_diff])
+    print(f'Result saved to {result_file}')
