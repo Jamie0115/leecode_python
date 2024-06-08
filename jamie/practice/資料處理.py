75f1ad8ea145f46195bdbba038ee45d555a700f2
Jamie0115 <gao880115@gmail.com>
2024-06-08 13:46:37 +0800

    資料處理

--- jamie/practice/資料處理.py
+++ jamie/practice/資料處理.py
@@ -1,9 +1,9 @@
-import pandas as pd
+import csv
 test1_path = "C:/Users/funny/Desktop/test1.csv"
 test2_path = "C:/Users/funny/Desktop/test2.csv"
-df1 = pd.read_csv("test1_path")
-df2 = pd.read_csv("test2_path")
-merged_df = pd.merge(df1, df2, on = "pixel", suffixes = ("_test1", "_test2"))
-merged_df["intensity_diff"] = merged_df["intensity_test1"]-merged_df["intensity_test2"]
-result_df = merged_df[["pixel", "intensity_diff"]]
-result_df.to_csv("result.csv", index = False)
+# 讀取 file1.csv
+with open(test1_path, mode='r', newline='') as test1:
+    reader1 = csv.reader(test1_path)
+    next(reader1)  # 跳過標題行
+    row1 = next(reader1)  # 第一筆資料
+    value1 = int(row1[1])  # 取得第一筆資料的 Value 欄位值
