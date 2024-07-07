ad020388e1b63bfdbca8a49e0a35a5ff0c9035da
Jamie0115 <gao880115@gmail.com>
2024-07-07 23:07:02 +0800

    class_Student

--- jamie/homework/homework_class.py
+++ jamie/homework/homework_class.py
@@ -1,4 +1,3 @@
-
 # 銀行帳戶
 # 設計一個銀行帳戶類別（Account），每個帳戶都有自己的帳號（account_number）、所有者的姓名（owner_name）、餘額（balance），並提供以下功能：
 
@@ -8,23 +7,61 @@
 # 轉賬（transfer），將款項從一個帳戶轉移到另一個帳戶
 
 class BankAccount:
-    def __init__(self, account_number, owner_name, balance):
-        self.account_number = account_number
-        self.owner_name = owner_name
-        self.balance = balance
+    def __init__(self, accountNumber: str, accountName: str, balance: int):
+        self.__accountNumber = accountNumber
+        self.__accountName = accountName
+        self.__balance = balance
+
+    # 存款（deposit）
     def deposit(self, amount):
-        return self.balance + amount
+        self.__balance += + amount
+        return self.__balance
+
+    # 提款（withdraw）
     def withdraw(self, amount):
-        return self.balance - amount
-    @staticmethod
-    def get_account_info():
-        return {
-            "Account Number : self.account_number,"
-            "Owner Name : {self.owner_name}",
-            "Balance : {self.balance}"
-        }
-    def transfer(self, target_account, amount):
-        if amount > self.balance:
-            return {"Insufficient funds"}
-        self.balance -= amount
-        target_account.balance += amount
+        self.__balance -= amount
+        return self.__balance
+
+    # 餘額查詢(balance)
+    def balance(self):  # READ
+        return self.__balance
+
+    def accountInfo(self):
+        return ("帳戶號碼: {accountNumber}, 帳戶名字: {accountName}, 帳戶餘額: {balance}"
+                .format(accountNumber = self.__accountNumber, accountName = self.__accountName, balance = self.__balance))
+
+    # 轉賬（transfer），將款項從一個帳戶轉移到另一個帳戶
+    def transfer(self, target_account: str, amount: int) -> str:
+        if target_account == self.__accountNumber:
+            return "目標帳戶不可與來源帳戶相同"
+
+        if self.__balance < amount:
+            return "金額不足"
+
+        self.__balance -= amount
+        return ("轉帳帳戶: {target_account}, 轉帳金額: {amount}, 帳戶餘額: {balance}"
+                .format(target_account = target_account, amount = amount, balance = self.__balance))
+
+
+bank = BankAccount("123456789000000", "Jamie", 100000)
+while True:
+    mode = int(input("檢查餘額:0, 存款:1, 提款:2, 帳戶訊息:3, 轉帳:4, 離開:5   請輸入:"))
+    if mode == 0:
+        print("目前存款餘額:" + str(bank.balance()))
+    elif mode == 1:
+        deposit_amount = int(input("請輸入存入的金額:"))
+        print("存入後的存款金額:", bank.deposit(deposit_amount))
+    elif mode == 2:
+        withdraw_amount = int(input("請輸入欲提款的金額:"))
+        print("提款後的餘額:", bank.withdraw(withdraw_amount))
+    elif mode == 3:
+        print("帳戶信息:", bank.accountInfo())
+    elif mode == 4:
+        target_account_number = input("轉帳帳戶號碼: ")
+        target_account_amount = int(input("轉帳金額: "))
+        print(bank.transfer(target_account_number, target_account_amount))
+    elif mode == 5:
+        print("謝謝,請取卡~")
+        break
+    else:
+        print("執行錯誤，請輸入正確數字")
--- jamie/homework/homework_class_1.py
+++ jamie/homework/homework_class_1.py
@@ -1,32 +1,72 @@
+"""
 import abc
-class Person:
+
+class Person(metaclass=abc.ABCMeta):
+    __role = "Person"
+
     def __init__(self, name: str):
-        self._name = name  # protected
+        self.__name = name
 
-    def printName(self):
-
-        print("My name is " + self._name + ".")
+    def __printName(self):
+        print("Hello, my name is " + self.__name + ".")
 
     @abc.abstractmethod
+    def _printRole(self):
+        return NotImplemented
+
     def introduce(self):
+        self.__printName()
+        self._printRole()
 
 
 class Student(Person):
+    __role = "Student"
+
     def __init__(self, name: str, number: int):
         super().__init__(name)
         self.number = number
 
+    def _printRole(self):
+        print("I am a {0}. My number is {1}.".format(self.__role, self.number))
+
 
 class Teacher(Person):
+    __role = "Teacher"
+
     def __init__(self, name: str, expertise: str):
         super().__init__(name)
         self.expertise = expertise
 
-    def printTeacherName(self):
-        print(self._name + " is my " + self.expertise + " teacher.")
+    def _printRole(self):
+        print("I am a {0}. My specialize is {1}.".format(self.__role, self.expertise))
 
-a = Student("Jamie", 1)
-b = Teacher("Barry", "physics")
+a = Teacher("Barry", "optical")
+b = Student("Jamie", 1)
 
-a.printName()
-b.printTeacherName()
+a.introduce()
+b.introduce()
+"""
+# 學生
+# 設計一個學生類別（Student），每個學生都有自己的學號（student_id）
+# 、姓名（name）、年級（grade）、和科目成績（subject_scores），並提供以下功能：
+# 新增一科成績（add_subject_score），可以新增一科科目的成績，包括科目名稱和成績。
+# 查詢全部科目成績（get_all_subject_scores），可以獲取該學生的所有科目成績。
+# 計算平均成績（calculate_average_score），可以計算該學生的平均成績。
+# 獲取學生資訊（get_student_info），可以獲取該學生的所有資訊，包括學號、姓名、年級、科目成績和平均成績。
+
+class Student:
+    def __init__(self, studentID: int, name: str, grade: int, subjectScore: float):
+        self.studentID = studentID
+        self.name = name
+        self.grade = grade
+        self.__subjectScore = subjectScore
+
+class SubjectScore:
+    def __init__(self, subject: str, subjectScore: float):
+        super().__init__(subjectScore)
+        self.subject = subject
+        self.scoreList = list()
+
+    def addScore(self, t: Student):
+        self.scoreList.append(t)
+
