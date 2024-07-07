2832f34f0ec114ba9c3c9a9016785bd82db2dd80
Eren <43052203+gmwu0330@users.noreply.github.com>
2024-07-07 23:54:27 +0800

    fix

--- jamie/oopDemo/StudentDemo.py
+++ jamie/oopDemo/StudentDemo.py
@@ -8,18 +8,6 @@ from functools import reduce
 # 查詢全部科目成績（get_all_subject_scores），可以獲取該學生的所有科目成績。
 # 計算平均成績（calculate_average_score），可以計算該學生的平均成績。
 # 獲取學生資訊（get_student_info），可以獲取該學生的所有資訊，包括學號、姓名、年級、科目成績和平均成績。
-class Subject:
-    def __init__(self, name: str, score: float):
-        self.__name = name
-        self.__score = score
-
-    def getName(self):
-        return self.__name
-
-    def getScore(self):
-        return self.__score
-
-
 class Student:
     def __init__(self, sid: int, name: str, grade: int):
         self.__sid = sid
@@ -27,19 +15,14 @@ class Student:
         self.__grade = grade
         self.__subjectScoreDict = dict()
 
-    def add_subject_score(self, subject: Subject) -> None:
-        self.__subjectScoreDict[subject.getName()] = subject
+    def add_subject_score(self, subject: str, score: float) -> None:
+        self.__subjectScoreDict[subject] = score
 
-    def get_all_subject_scores(self) -> list:
-        returnList = list()
-        for subject in self.__subjectScoreDict.values():
-            returnList.append(subject.getName() + ":" + str(subject.getScore()))
-        return returnList
+    def get_all_subject_scores(self) -> dict:
+        return self.__subjectScoreDict
 
     def calculate_average_score(self) -> float:
-        allSubject = self.__subjectScoreDict.values()
-        total = reduce(lambda x, y: x.getScore() + y.getScore(), allSubject)
-        return total / len(self.__subjectScoreDict)
+        return sum(self.__subjectScoreDict.values()) / len(self.__subjectScoreDict)
 
     def get_student_info(self) -> dict:
         return {
@@ -52,7 +35,9 @@ class Student:
 
 
 a = Student(1, "Eren", 3)
-a.add_subject_score(Subject("Math", 90.5))
-a.add_subject_score(Subject("English", 60.5))
+a.add_subject_score("Math", 90.5)
+a.add_subject_score("English", 60.5)
 
+print(a.get_all_subject_scores())
+print(a.calculate_average_score())
 print(a.get_student_info())
